29714eab5eff94e5c1821cb451549f01c8085a15
kagw95 <aleksandr.kapustin.qubership@gmail.com>
2025-06-24 11:25:15 +0400

    Explicit init of Velocity Tools is added into VelocityTemplateEngine constructor. Unit tests are added for $math, $date, $number, $esc and $xml tools.

--- mockingbird-triggers-camel/mockingbird-camel-common/src/main/java/org/qubership/automation/itf/trigger/template/velocity/VelocityTemplateEngine.java
+++ mockingbird-triggers-camel/mockingbird-camel-common/src/main/java/org/qubership/automation/itf/trigger/template/velocity/VelocityTemplateEngine.java
@@ -20,6 +20,8 @@ package org.qubership.automation.itf.trigger.template.velocity;
 import static org.qubership.automation.itf.core.util.constants.InstanceSettingsConstants.VELOCITY_CONFIG;
 
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
@@ -28,6 +30,10 @@ import org.apache.velocity.context.Context;
 import org.apache.velocity.exception.VelocityException;
 import org.apache.velocity.runtime.directive.Directive;
 import org.apache.velocity.tools.ToolManager;
+import org.apache.velocity.tools.config.Data;
+import org.apache.velocity.tools.config.FactoryConfiguration;
+import org.apache.velocity.tools.config.ToolConfiguration;
+import org.apache.velocity.tools.config.ToolboxConfiguration;
 import org.qubership.automation.itf.core.model.common.Storable;
 import org.qubership.automation.itf.core.model.jpa.context.JsonContext;
 import org.qubership.automation.itf.core.util.config.Config;
@@ -63,8 +69,113 @@ public class VelocityTemplateEngine implements TemplateEngine {
         for (Class directiveClass : ClassResolver.getInstance().getSubtypesOf(Directive.class)) {
             engine.loadDirective(directiveClass.getName());
         }
+        /*
+            Explicit init of Velocity Tools 2.0 is implemented,
+            instead of bundled tools.xml files parsing
+            (after commons-beanutils upgrade to 1.9.4).
+         */
+        FactoryConfiguration factoryConfiguration = makeGenericFactoryConfig();
+        factoryConfiguration.addConfiguration(makeStrutsFactoryConfig());
+        factoryConfiguration.addConfiguration(makeViewFactoryConfig());
+
         toolManager = new ToolManager();
+        toolManager.getToolboxFactory().configure(factoryConfiguration);
         toolManager.setVelocityEngine(engine);
+    }
+
+    private static Data fillData(String type, String key, Object value) {
+        Data data = new Data();
+        data.setType(type);
+        data.setKey(key);
+        data.setValue(value);
+        return data;
+    }
+
+    private static List<ToolConfiguration> makeToolsList(String... args) {
+        List<ToolConfiguration> list = new ArrayList<>();
+        for(String arg : args) {
+            ToolConfiguration cfg = new ToolConfiguration();
+            cfg.setClassname(arg);
+            list.add(cfg);
+        }
+        return list;
+    }
+
+    private static FactoryConfiguration makeGenericFactoryConfig() {
+        FactoryConfiguration factoryConfiguration = new FactoryConfiguration();
+        factoryConfiguration.addData(fillData("number", "TOOLS_VERSION", "2.0"));
+        factoryConfiguration.addData(fillData("boolean", "GENERIC_TOOLS_AVAILABLE", "true"));
+
+        ToolboxConfiguration applicationToolboxConfiguration = new ToolboxConfiguration();
+        applicationToolboxConfiguration.setScope("application");
+        applicationToolboxConfiguration.setTools(makeToolsList(
+                "org.apache.velocity.tools.generic.AlternatorTool",
+                "org.apache.velocity.tools.generic.ClassTool",
+                "org.apache.velocity.tools.generic.ComparisonDateTool",
+                "org.apache.velocity.tools.generic.ConversionTool",
+                "org.apache.velocity.tools.generic.DisplayTool",
+                "org.apache.velocity.tools.generic.EscapeTool",
+                "org.apache.velocity.tools.generic.FieldTool",
+                "org.apache.velocity.tools.generic.MathTool",
+                "org.apache.velocity.tools.generic.NumberTool",
+                "org.apache.velocity.tools.generic.ResourceTool",
+                "org.apache.velocity.tools.generic.SortTool",
+                "org.apache.velocity.tools.generic.XmlTool"));
+        factoryConfiguration.addToolbox(applicationToolboxConfiguration);
+
+        ToolboxConfiguration requestToolboxConfiguration = new ToolboxConfiguration();
+        requestToolboxConfiguration.setScope("request");
+        requestToolboxConfiguration.setTools(makeToolsList(
+                "org.apache.velocity.tools.generic.ContextTool",
+                "org.apache.velocity.tools.generic.LinkTool",
+                "org.apache.velocity.tools.generic.LoopTool",
+                "org.apache.velocity.tools.generic.RenderTool"));
+        factoryConfiguration.addToolbox(requestToolboxConfiguration);
+        return factoryConfiguration;
+    }
+
+    private static FactoryConfiguration makeStrutsFactoryConfig() {
+        FactoryConfiguration factoryConfiguration = new FactoryConfiguration();
+        factoryConfiguration.addData(fillData("boolean", "STRUTS_TOOLS_AVAILABLE", "true"));
+
+        ToolboxConfiguration requestToolboxConfiguration = new ToolboxConfiguration();
+        requestToolboxConfiguration.setScope("request");
+        requestToolboxConfiguration.setTools(makeToolsList(
+                "org.apache.velocity.tools.struts.ActionMessagesTool",
+                "org.apache.velocity.tools.struts.ErrorsTool",
+                "org.apache.velocity.tools.struts.FormTool",
+                "org.apache.velocity.tools.struts.MessageTool",
+                "org.apache.velocity.tools.struts.StrutsLinkTool",
+                "org.apache.velocity.tools.struts.TilesTool",
+                "org.apache.velocity.tools.struts.ValidatorTool"));
+        factoryConfiguration.addToolbox(requestToolboxConfiguration);
+        return factoryConfiguration;
+    }
+
+    private static FactoryConfiguration makeViewFactoryConfig() {
+        FactoryConfiguration factoryConfiguration = new FactoryConfiguration();
+        factoryConfiguration.addData(fillData("boolean", "VIEW_TOOLS_AVAILABLE", "true"));
+
+        ToolboxConfiguration requestToolboxConfiguration = new ToolboxConfiguration();
+        requestToolboxConfiguration.setScope("request");
+        requestToolboxConfiguration.setTools(makeToolsList(
+                "org.apache.velocity.tools.view.CookieTool",
+                "org.apache.velocity.tools.view.ImportTool",
+                "org.apache.velocity.tools.view.IncludeTool",
+                "org.apache.velocity.tools.view.LinkTool",
+                "org.apache.velocity.tools.view.PagerTool",
+                "org.apache.velocity.tools.view.ParameterTool",
+                "org.apache.velocity.tools.view.ViewContextTool",
+                "org.apache.velocity.tools.generic.ResourceTool"));
+        factoryConfiguration.addToolbox(requestToolboxConfiguration);
+
+        ToolboxConfiguration sessionToolboxConfiguration = new ToolboxConfiguration();
+        sessionToolboxConfiguration.setScope("session");
+        sessionToolboxConfiguration.setProperty("createSession", "false");
+        sessionToolboxConfiguration.setTools(makeToolsList(
+                "org.apache.velocity.tools.view.BrowserTool"));
+        factoryConfiguration.addToolbox(sessionToolboxConfiguration);
+        return factoryConfiguration;
     }
 
     /**
---
+++ mockingbird-triggers-camel/mockingbird-camel-common/src/test/java/org/qubership/automation/itf/trigger/template/velocity/VelocityTemplateEngineTest.java
@@ -0,0 +1,78 @@
+package org.qubership.automation.itf.trigger.template.velocity;
+
+import java.util.Map;
+
+import org.apache.commons.lang.StringEscapeUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.qubership.automation.itf.core.model.common.Storable;
+import org.qubership.automation.itf.core.model.jpa.context.InstanceContext;
+import org.qubership.automation.itf.core.model.jpa.context.TcContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.testng.Assert;
+
+import com.google.common.collect.Maps;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath*:*template-velocity-test-context.xml"})
+public class VelocityTemplateEngineTest {
+    private VelocityTemplateEngine engine;
+
+    @Before
+    public void setUp() {
+        engine = new VelocityTemplateEngine();
+    }
+
+    @Test
+    public void testMathTool() {
+        String sourceString = "$math.random(100,999)";
+        TcContext context = new TcContext();
+        Map<String, Storable> map = Maps.newHashMap();
+        String processed = engine.process(map, sourceString, InstanceContext.from(context, null));
+        Assert.assertNotEquals(processed, sourceString);
+    }
+
+    @Test
+    public void testDateTool() {
+        String sourceString = "#set($startDate = $date.format('yyyy-MM-dd''T''HH:mm:ss', $date))\n$startDate";
+        TcContext context = new TcContext();
+        Map<String, Storable> map = Maps.newHashMap();
+        String processed = engine.process(map, sourceString, InstanceContext.from(context, null));
+        Assert.assertFalse(processed.contains("$date"));
+        System.out.println(processed);
+    }
+
+    @Test
+    public void testEscTool() {
+        String htmlString = "Some String with html/xml tags: <customer><name>Alex</name><id>54321</id></customer>";
+        String velocityString = "$esc.html(\"" + htmlString + "\")";
+        TcContext context = new TcContext();
+        Map<String, Storable> map = Maps.newHashMap();
+        String processed = engine.process(map, velocityString, InstanceContext.from(context, null));
+        Assert.assertEquals(processed, StringEscapeUtils.escapeHtml(htmlString));
+    }
+
+    @Test
+    public void testNumberTool() {
+        String velocityString = "#set($num = 55.666)\n$number.format(\"#0000\", $num)";
+        TcContext context = new TcContext();
+        Map<String, Storable> map = Maps.newHashMap();
+        String processed = engine.process(map, velocityString, InstanceContext.from(context, null));
+        Assert.assertEquals(processed, "0056");
+    }
+
+    @Test
+    public void testXmlTool() {
+        String velocityString = "#set($myXML = $xml.parse($tc.response))\n"
+                + "#foreach($eventFragment in $myXML.children().iterator())\n"
+                + "$eventFragment\n"
+                + "#end";
+        TcContext context = new TcContext();
+        context.put("response", "<note><to>Tove</to><from>Jani</from><heading>Reminder</heading><body>Don't forget me this weekend!</body></note>");
+        Map<String, Storable> map = Maps.newHashMap();
+        String processed = engine.process(map, velocityString, InstanceContext.from(context, null));
+        Assert.assertEquals(processed, "<to>Tove</to>\n<from>Jani</from>\n<heading>Reminder</heading>\n<body>Don't forget me this weekend!</body>\n");
+    }
+}
