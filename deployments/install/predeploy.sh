92d781a0ff6928214239eab65858eaf7de50f8ff
dale1020 <daria.s.lebedeva@netcracker.com>
2025-08-26 11:46:55 +0500

    add CI workflow

--- deployments/install/predeploy.sh
+++ deployments/install/predeploy.sh
@@ -9,11 +9,12 @@ fi
 . ./atp-common-scripts/openshift/common.sh
 
 _ns="${NAMESPACE}"
-echo "***** Preparing MongoDB connection *****"
-EDS_GRIDFS_DB="$(env_default "${EDS_GRIDFS_DB}" "itf_eds_gridfs" "${_ns}")"
-EDS_GRIDFS_USER="$(env_default "${EDS_GRIDFS_USER}" "itf_eds_gridfs" "${_ns}")"
-EDS_GRIDFS_PASSWORD="$(env_default "${EDS_GRIDFS_PASSWORD}" "itf_eds_gridfs" "${_ns}")"
+if [ "${EI_GRIDFS_ENABLED:-true}" = "true" ]; then
+  echo "***** Preparing MongoDB connection *****"
+  EDS_GRIDFS_DB="$(env_default "${EDS_GRIDFS_DB}" "itf_eds_gridfs" "${_ns}")"
+  EDS_GRIDFS_USER="$(env_default "${EDS_GRIDFS_USER}" "itf_eds_gridfs" "${_ns}")"
+  EDS_GRIDFS_PASSWORD="$(env_default "${EDS_GRIDFS_PASSWORD}" "itf_eds_gridfs" "${_ns}")"
 
-echo "***** Initializing databases ******"
-init_mongo "${MONGO_DB_ADDR}" "${EDS_GRIDFS_DB}" "${EDS_GRIDFS_USER}" "${EDS_GRIDFS_PASSWORD}" "${MONGO_DB_PORT}"  "${mongo_user}" "${mongo_pass}"
-
+  echo "***** Initializing databases ******"
+  init_mongo "${MONGO_DB_ADDR}" "${EDS_GRIDFS_DB}" "${EDS_GRIDFS_USER}" "${EDS_GRIDFS_PASSWORD}" "${MONGO_DB_PORT}"  "${mongo_user}" "${mongo_pass}"
+fi
