bede605a4b60c8fe1e025f4ad90223001141ed74
dmch1019 <dmitrii.churbanov@netcracker.com>
2026-03-30 17:39:18 +0500

    feat: add disabled ssl for feign client configuration;

--- stubs-aggregator/src/main/java/org/qubership/automation/itf/conf/OkHttpFeignConfiguration.java
+++ stubs-aggregator/src/main/java/org/qubership/automation/itf/conf/OkHttpFeignConfiguration.java
@@ -17,17 +17,45 @@
 
 package org.qubership.automation.itf.conf;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import feign.okhttp.OkHttpClient;
+import okhttp3.OkHttpClient;
 
 @Configuration
 public class OkHttpFeignConfiguration {
 
     @Bean
-    public OkHttpClient client() {
-        return new OkHttpClient();
+    public feign.Client feignClient(okhttp3.OkHttpClient okHttpClient) {
+        return new feign.okhttp.OkHttpClient(okHttpClient);
+    }
+
+    @Bean
+    public OkHttpClient client() throws NoSuchAlgorithmException, KeyManagementException {
+        TrustManager[] trustAllCerts = new TrustManager[]{
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[0]; }
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+                }
+        };
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, trustAllCerts, new SecureRandom());
+
+        return new OkHttpClient.Builder()
+                .sslSocketFactory(sslContext.getSocketFactory(), (X509TrustManager) trustAllCerts[0])
+                .hostnameVerifier((hostname, session) -> true)
+                .build();
     }
 
 }
